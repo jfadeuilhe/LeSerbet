4a68a766ceae4eadfb9a5a6a05997a597b31dc61
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-16 09:49:20 +0200

    Update #13

--- GeStock/WebFolder/index.waPage/scripts/index-smartphone.js
+++ GeStock/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -2,7 +2,10 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var bClose = {};	// @buttonImage
+	var iEfface = {};	// @image
+	var iPrec = {};	// @image
+	var iFerme = {};	// @image
+	var iSuiv = {};	// @image
 	var dgStock = {};	// @dataGrid
 	var id_Login = {};	// @login
 	var documentEvent = {};	// @document
@@ -11,13 +14,29 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	
 	var srchTimeout; 	//Pour gérer un délais avant recherche
 	var $art = $("#cArt");
+	var $noId = $("#id_NoId");
 	var $ouvreArt = $("#cDetails");
 
 // eventHandlers// @lock
 
-	bClose.click = function bClose_click (event)// @startlock
+	iEfface.click = function iEfface_click (event)// @startlock
+	{// @endlock
+		$$("id_txtArt").setValue("");
+	};// @lock
+
+	iPrec.click = function iPrec_click (event)// @startlock
+	{// @endlock
+		sources.articles.selectPrevious();
+	};// @lock
+
+	iFerme.click = function iFerme_click (event)// @startlock
 	{// @endlock
 		$ouvreArt.hide();
+	};// @lock
+
+	iSuiv.click = function iSuiv_click (event)// @startlock
+	{// @endlock
+		sources.articles.selectNext();
 	};// @lock
 
 	dgStock.onRowDblClick = function dgStock_onRowDblClick (event)// @startlock
@@ -28,12 +47,14 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	id_Login.logout = function id_Login_logout (event)// @startlock
 	{// @endlock
 		$art.hide();
+		$noId.show();
 	};// @lock
 
 	id_Login.login = function id_Login_login (event)// @startlock
 	{// @endlock
 		selArticles();
 		$art.show();
+		$noId.hide();
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
@@ -42,9 +63,11 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		$ouvreArt.hide();
 		if(user!=null) {
 			$art.show();
+			$noId.hide();
 			selArticles();
 		}else{
 			$art.hide();
+			$noId.show();
 		}
 
 	};// @lock
@@ -62,18 +85,19 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		
 		if(tmpTxt!=""){
 		
-			txtSrch = "(Epuisé = false) && "+txt2Srch_OU_ET(tmpTxt,"Désignation,Titre_Fournis,NomFournisseur,Millésime");//,Couleur,Code_Cru_A.Village");
-
+			txtSrch = "(Epuisé = false) && "+txt2Srch_OU_ET(tmpTxt,"Désignation,Titre_Fournis,NomFournisseur,Millésime,Couleur,Code_Cru_A.Village,Code_Cru_A.Secteur,Code_Cru_A.Région_Appellation");
+			
+			//alert("Recherche : "+txtSrch);
 		} 
-		
-		//alert("Recherche : "+txtSrch);
 		
 		sources.articles.query(txtSrch);
 		
-		//sources.articles.query(txtSrch); 	//'Titre = :1 || Nom = :1 || Contact = :1',{ params: ["*" + tmpTxt + "*"]});
 	};
 // @region eventManager// @startlock
-	WAF.addListener("bClose", "click", bClose.click, "WAF");
+	WAF.addListener("iEfface", "click", iEfface.click, "WAF");
+	WAF.addListener("iPrec", "click", iPrec.click, "WAF");
+	WAF.addListener("iFerme", "click", iFerme.click, "WAF");
+	WAF.addListener("iSuiv", "click", iSuiv.click, "WAF");
 	WAF.addListener("dgStock", "onRowDblClick", dgStock.onRowDblClick, "WAF");
 	WAF.addListener("id_Login", "logout", id_Login.logout, "WAF");
 	WAF.addListener("id_Login", "login", id_Login.login, "WAF");
