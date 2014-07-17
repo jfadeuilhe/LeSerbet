da89cb6ecdb7bae9b4bb7973d4850dbb9455ad75
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-07-17 12:02:38 +0200

    Update #21

--- GeStock/Modules/cf_bib.js
+++ GeStock/Modules/cf_bib.js
@@ -1,8 +1,4 @@
-﻿/*	In order to make the helloWorld() function available client-side, you have to add a reference to the 'cf_bib' module in the GUI Designer.
-	The helloWorld() function can be executed from your JS file as follows:
-	alert(cf_bib.helloWorld());
-	
-	For more information, refer to http://doc.wakanda.org/Wakanda0.Beta/help/Title/en/page1516.html
+﻿/*	Camille's méthodes
 */
 
 exports.dupliquer = function dupliquer(src){
